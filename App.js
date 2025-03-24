bd5c3718a71119c7b7d769c66cf67ec9c37ad389
chasemckay <michaelchasemckay@gmail.com>
2025-03-24 11:19:07 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,6 @@
-// App component placeholder
+import { HeroSection } from "./components/ui/HeroSection";
+import { Navbar } from "./components/ui/Navbar";
+import { PricingTable } from "./components/ui/PricingTable";
+import { Footer } from "./components/ui/Footer";
+import { FeaturesSection } from "./components/ui/FeaturesSection";
+import { Testimonials } from "./components/ui/Testimonials";
